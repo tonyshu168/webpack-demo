903172936b1d4b159e663817173dc5d07a2677b3
tonyshu168 <493397276@qq.com>
2020-12-13 15:29:38 +0800

    add handler reactjs

--- src/index.js
+++ src/index.js
@@ -1,33 +1,11 @@
 // index.js
 import '@babel/polyfill';
 
-import css from './index.css';
-import './index.less';
+import React, { Components } from 'react';
+import ReactDom from 'react-dom';
 
-import pic from '../public/images/png01.png';
-
-const img = new Image();
-img.src = pic;
-img.classList.add('logo');
-document.getElementById('root').append(img);
-
-const json = require('./index.json');
-import { add, increment } from './other.js';
-console.log(json, add(2, 3));
-console.log(increment( 5 ));
-
-import axios from 'axios';
-
-axios.get('/api/info').then(res => { 
-  console.log(res)
-})
-
-var btn = document.createElement('button');
-btn.innerHTML = '新增';
-document.getElementById('root').appendChild(btn);
-
-btn.onclick = function() {
-  const div = document.createElement('div');
-  div.innerHTML = 'item';
-  document.getElementById('root').appendChild(div);
+function App() {
+  return <div>Hello react</div>
 }
+
+ReactDom.render(<App />, document.getElementById('root'));
